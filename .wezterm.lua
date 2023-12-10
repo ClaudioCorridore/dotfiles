1292f74d9df66ab29934d2be5c9994cb03a6a33e
Claudio Corridore <claudio.corridore@gmail.com>
2023-12-10 07:52:07 +0000

    feat(wezterm): add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,36 @@
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+
+config.color_scheme = 'Night Owl (Gogh)'
+
+config.font = wezterm.font_with_fallback {
+  { family = 'Monaspace Krypton', weight = 'Medium', harfbuzz_features = { 'ss01=0', 'ss02=0', 'ss03=0', 'ss04=0', 'ss05=0', 'ss06=0', 'ss07=0', 'ss08=0', 'calt=1', 'dlig=0' } },
+  { family = 'MonaspiceKr NF', weight = 'Medium' },
+  'JetBrains Mono'
+}
+
+config.font_size = 14.0
+
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_padding = {
+  left = '0.5cell',
+  right = '0.5cell',
+  top = '0',
+  bottom = '0.5cell',
+}
+
+config.default_cursor_style = 'BlinkingUnderline'
+
+return config
+
